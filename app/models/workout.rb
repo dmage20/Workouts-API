d42bfa8bb574a5130c6a91493a96f2d4085b9a7f
Daniel Mage <dmage20@gmail.com>
2019-04-08 16:51:03 +0700

    can display workouts with query

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,8 @@
 class Workout < ApplicationRecord
+    include PgSearch
+    pg_search_scope :search_workouts_for_keyword,
+      against: [ :description ],
+      using: {
+        tsearch: { prefix: true } # <-- 
+      }
 end
